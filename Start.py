f9caf252585a13c0e1cf283bc96d65e52ffb2fd8
H0NEP0T-466 <fa-23-bscs-466@lgu.edu.pk>
2025-08-18 02:19:58 +0500

    seeding

--- Start.py
+++ Start.py
@@ -120,3 +120,99 @@ print(np.sum(data))          # Total sum → 21
 print(np.sum(data, axis=0))  # Column-wise → [5 7 9]
 print(np.sum(data, axis=1))  # Row-wise → [6 15]
 
+
+import numpy as np
+
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+
+print(np.dot(a, b))   # 1*4 + 2*5 + 3*6 = 32
+print(a @ b)          # Same as dot
+
+
+A = np.array([[1, 2],
+              [3, 4]])
+B = np.array([[5, 6],
+              [7, 8]])
+
+print(np.matmul(A, B))
+# [[19 22]
+#  [43 50]]
+
+M = np.array([[1, 2, 3],
+              [4, 5, 6]])
+
+print(M.T)
+# [[1 4]
+#  [2 5]
+#  [3 6]]
+
+A = np.array([[1, 2],
+              [3, 4]])
+
+print(np.linalg.det(A))   # Determinant = -2.0
+print(np.linalg.inv(A))   # Inverse
+# [[-2.   1. ]
+#  [ 1.5 -0.5]]
+
+v = np.array([3, 4])
+print(np.linalg.norm(v))  # √(3²+4²) = 5
+
+
+A = np.array([[4, -2],
+              [1,  1]])
+
+values, vectors = np.linalg.eig(A)
+
+print("Eigenvalues:", values)
+print("Eigenvectors:\n", vectors)
+
+
+print("Random numbers:")
+
+arr = np.random.rand(5,2)  
+print(arr)
+
+print("normal distrubution:")
+
+arr = np.random.randn(10)  # 1000 random numbers (mean=0, std=1)
+
+print(arr)
+print(np.mean(arr))  # ≈ 0 (close to mean)
+print(np.std(arr))   # ≈ 1 (close to std)
+
+
+print("Random integers:")
+np.random.seed(4)
+arr = np.random.randint(1, 10, size=5)  # 5 numbers between 1–9
+print(arr)   # e.g., [3 7 1 9 4]
+
+
+print("Random seed:")
+np.random.seed(42)
+print(np.random.randn(3))  # Always the same numbers
+
+
+
+print("Random choice:")
+nums = np.array([10, 20, 30, 40, 50])
+
+print(np.random.choice(nums))            # One random pick
+print(np.random.choice(nums, 3))         # Pick 3 random values
+print(np.random.choice(nums, 3, replace=False))  # 3 unique picks
+
+
+
+print("seeding")
+np.random.seed(42)
+print(np.random.rand(3))  # Always the same numbers
+
+print("seeding loop")
+
+np.random.seed(0)
+print(np.random.randint(1, 10, size=5))
+print("hel")
+for i in range(5):
+    np.random.seed(i)
+    print(np.random.randint(1, 10, size=5))
+
