52bb5249c5db008b35cf8241303e2ae50acc291f
H0NEP0T-466 <fa-23-bscs-466@lgu.edu.pk>
2025-08-22 17:54:53 +0500

    pandas

--- Pandas.py
+++ Pandas.py
@@ -8,12 +8,35 @@ data = {
     'Age': [20, 22, 19],
     'Marks': [88, 92, 79]
 }
+
+maindata=p.read_csv("data.csv",encoding="latin1")
+
 df = p.DataFrame(data)
 print(df)
+print("----")
+print(df.loc[0])  # First row
+print("----")
+print(df.loc[1, 'Name'])
+print("----")
+print(df.loc[1:2])
+print("CSV DATA")
+# print(maindata.loc[:, 'Engines'])
+print(maindata.shape)     # (10, 11) → 10 rows, 11 columns
+print(maindata.columns)   # all column names
+print(maindata.index)     # row index (0–9)
+print(maindata.dtypes)    # data types of each column
+
+print(maindata.loc[0])                 # first row (by label)
+print(maindata.iloc[0, 1])             # first row, 2nd column → "SF90 STRADALE"
+print(maindata.loc[:, ["Company Names","Cars Names","Engines"]])  # only these cols
+print(maindata.iloc[0:3, 0:3])         # first 3 rows, first 3 cols
 
 
-data=p.read_csv("data.csv",encoding="latin1")
-print(data)
-print(data.index)
-print(data.info())
-print(data.describe())
+
+# Cars with 2 seats
+print("----")
+print(maindata[maindata["Seats"] == 2])
+
+
+
+
