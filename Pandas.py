1e344d7da2b5623c70962d914d9cde881a0972fd
H0NEP0T-466 <fa-23-bscs-466@lgu.edu.pk>
2025-08-22 17:06:40 +0500

    pandas

--- Pandas.py
+++ Pandas.py
@@ -2,3 +2,16 @@ import pandas as p
 
 data=p.Series([1, 2, 3, 4, 5],index=['a','b','c','d','e'])
 print(data)
+
+data = {
+    'Name': ['Ali', 'Umer', 'Bilal'],
+    'Age': [20, 22, 19],
+    'Marks': [88, 92, 79]
+}
+df = p.DataFrame(data)
+print(df)
+
+
+data=p.read_csv("data.csv",encoding="latin1")
+print(data)
+
