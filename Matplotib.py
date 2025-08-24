858c52666c609af1247c372ff97d01cba8c7b42e
H0NEP0T-466 <fa-23-bscs-466@lgu.edu.pk>
2025-08-24 16:26:48 +0500

    matplotli

--- Matplotib.py
+++ Matplotib.py
@@ -1,4 +1,24 @@
-import matplotlib as m
+import matplotlib.pyplot as plt
 
+# Data
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 6, 8, 10]
+
+# Basic line plot
+plt.plot(x, y, label="y = 2x")
+
+# Titles & labels
+plt.title("Basic Line Plot")
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+
+# Legend
+plt.legend()
+
+# Save the figure
+#plt.savefig("plot.png")
+
+# Show plot
+plt.show()
 
 
