9ec85dbf4fc8dd0615a35ad9351f311fd2e02479
H0NEP0T-466 <fa-23-bscs-466@lgu.edu.pk>
2025-08-27 15:03:33 +0500

    plot

--- Matplotib.py
+++ Matplotib.py
@@ -49,10 +49,72 @@ plt.show()
 
 fig3=plt.gcf()  # Get the current figure
 plt.close("fig3")
-data = np.random.randn(1000)  
+#data = np.random.randn(1000)  
 
-plt.hist(data, bins=30, color="purple", edgecolor="black")
+data=[1, 2, 2, 3, 3, 3, 4, 4, 4, 4]
+plt.hist(data, bins=10, color="purple", edgecolor="black")
 plt.xlabel("Value")
 plt.ylabel("Frequency")
 plt.title("Histogram Example")
 plt.show()
+
+
+fig4=plt.gcf()  # Get the current figure
+plt.close("fig4")
+
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 6, 8, 10]
+
+plt.plot(x, y, 
+         color="red",        # line color
+         marker="o",         # circle marker
+         linestyle="--",     # dashed line
+         linewidth=2, 
+         markersize=8,
+         label="Styled Line")
+
+plt.title("Customized Line Plot")
+plt.legend()
+plt.show()
+
+fig5=plt.gcf()  # Get the current figure
+plt.close("fig5")
+
+
+plt.plot(x, y, marker="s", color="green")
+
+plt.xlim(0, 6)              # x-axis range
+plt.ylim(0, 12)             # y-axis range
+plt.xticks([0, 2, 4, 6])    # custom ticks
+plt.yticks([0, 5, 10])      
+
+plt.title("Axis Customization Example")
+plt.show()
+
+fig6=plt.gcf()  # Get the current figure
+plt.close("fig6")
+
+# Method 1: subplot()
+plt.subplot(1, 2, 1)   # 1 row, 2 cols, first plot
+plt.plot(x, y, color="blue")
+plt.title("Plot 1")
+
+plt.subplot(1, 2, 2)   # second plot
+plt.plot(y, x, color="orange")
+plt.title("Plot 2")
+
+plt.tight_layout()
+plt.show()
+
+# Method 2: subplots()
+fig, ax = plt.subplots(2, 1, figsize=(6,6))  # 2 rows, 1 col
+ax[0].plot(x, y, color="purple")
+ax[0].set_title("First Plot")
+
+ax[1].scatter(y, x, color="brown")
+ax[1].set_title("Second Plot")
+
+plt.tight_layout()
+plt.show()
+
+
