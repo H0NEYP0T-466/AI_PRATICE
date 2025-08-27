62e8657f88a0aa90555c124c0f38882a3817f782
H0NEP0T-466 <fa-23-bscs-466@lgu.edu.pk>
2025-08-27 14:49:41 +0500

    plot

--- Matplotib.py
+++ Matplotib.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 x = [1, 2, 3, 4, 5]
 y1 = [2, 4, 6, 8, 10]
@@ -14,4 +15,44 @@ plt.xlabel("X-axis")
 plt.ylabel("Y-axis")
 
 plt.legend()   
+plt.show()
+fig1=plt.gcf()  # Get the current figure
+#plt.savefig("legend_example.png")  # Save the figure as a PNG file
+
+plt.close("fig1")
+
+epochs = [1, 2, 3, 4, 5]
+train_loss = [0.9, 0.7, 0.5, 0.4, 0.3]
+val_loss = [1.0, 0.8, 0.6, 0.55, 0.5]
+
+plt.plot(epochs, train_loss, label="Training Loss")
+plt.plot(epochs, val_loss, label="Validation Loss")
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+plt.title("Training vs Validation Loss")
+plt.legend()
+plt.show()
+
+
+fig2 = plt.gcf()  # Get the current figure
+plt.close("fig2")
+x = np.random.rand(50)
+y = np.random.rand(50)
+
+plt.scatter(x, y, c="blue", marker="o", label="Data Points")
+plt.xlabel("X Feature")
+plt.ylabel("Y Feature")
+plt.title("Scatter Plot Example")
+plt.legend()
+plt.show()
+
+
+fig3=plt.gcf()  # Get the current figure
+plt.close("fig3")
+data = np.random.randn(1000)  
+
+plt.hist(data, bins=30, color="purple", edgecolor="black")
+plt.xlabel("Value")
+plt.ylabel("Frequency")
+plt.title("Histogram Example")
 plt.show()
