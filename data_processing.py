bc7b5ea51482fe99610f2f559b8c8be51cbf8bf8
H0NEP0T-466 <fa-23-bscs-466@lgu.edu.pk>
2025-08-29 16:52:26 +0500

    feature scalling

--- data_processing.py
+++ data_processing.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import sklearn as sk
 from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler,MinMaxScaler
 
 data=pd.read_csv('student_dataset.csv')
 copy=data.copy()
@@ -16,3 +17,21 @@ print(copy[['Gender','Passed']].head(10))
 copy_encoded=pd.get_dummies(copy,columns=['Year'],dtype=int)
 print("one hot encoding")
 print(copy_encoded.head(10))
+
+#feature scaling
+
+data={
+    "studyHours":[1,2,3,4,5],
+    "marks":[120,150,170,200,250]
+}
+objectofstandardscaler=StandardScaler()
+objectofmixmax=MinMaxScaler()
+
+
+new_data=pd.DataFrame(data)
+standardvalue=objectofstandardscaler.fit_transform(new_data)
+minmaxvalue=objectofmixmax.fit_transform(new_data)
+print("standard value is:")
+print(standardvalue)
+print("min max value is:")
+print(minmaxvalue)
