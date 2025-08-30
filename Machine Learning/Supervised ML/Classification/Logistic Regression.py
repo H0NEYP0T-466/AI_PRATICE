b181980c71f1e50c04a39cd00475e6b92054b448
H0NEP0T-466 <fa-23-bscs-466@lgu.edu.pk>
2025-08-30 15:50:07 +0500

    logistic regression

---
+++ Machine Learning/Supervised ML/Classification/Logistic Regression.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+
+data = {
+    "studyHours": [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+        11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        21, 22, 23, 24, 25, 26, 27, 28, 29, 30
+    ],
+    "passExam": [
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+    ]
+}
+
+
+
+df = pd.DataFrame(data)
+X = df[["studyHours"]]
+y = df["passExam"]
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
+
+hours = float(input("Enter the number of study hours: "))
+
+
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+
+predicted_pass = model.predict(pd.DataFrame([[hours]], columns=["studyHours"]))
+print(f"Predicted pass status for {hours} study hours: {predicted_pass[0]}")
+
+y_pred = model.predict(X_test)
+r2 = r2_score(y_test, y_pred)
+print("R² Score:", r2)
+
+plt.scatter(X, y, color="blue", label="Actual Data")         
+plt.plot(X,model.predict(X), color="red", label="Regression Line") 
+
+
+plt.scatter(hours, predicted_pass, color="green", s=100, label="Predicted Point")
+
+plt.xlabel("Study Hours")
+plt.ylabel("Pass Exam")
+plt.title("Logistic Regression - Study Hours vs Pass Exam")
+plt.legend()
+plt.show()
